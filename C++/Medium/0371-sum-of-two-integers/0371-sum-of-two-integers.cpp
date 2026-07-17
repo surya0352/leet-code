68efdeea19d737648159dba90623c96487606181
surya0352 <surya.25b01010352@abes.ac.in>
2026-07-17 23:46:44 +0530

    Math 0371-sum-of-two-integers .cpp Medium 07-17-2026

--- C++/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ C++/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+          while (b != 0) {
+            unsigned carry = (unsigned)(a & b);
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
     }
 };
