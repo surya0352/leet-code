3bd4a6bb2fce802f29917a8256902b587047db40
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-07-23 19:25:45 +0530

    1768-merge-strings-alternately Easy .cpp

--- C++/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ C++/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans = "";
-        int n1 = word1.length();
-        int n2 = word2.length();
-        int i = 0, j = 0;
-        while (i < n1 && j < n2) {
-            ans += word1[i];
-            ans += word2[j];
-            i++;
-            j++;
+        string res = "";
+        const int n1 = word1.size(), n2 = word2.size();
+        for (int i = 0; i < max(n1, n2); i++) {
+            if (i < n1)
+                res += word1[i];
+            if (i < n2)
+                res += word2[i];
         }
-
-        while (i < n1) {
-            ans += word1[i];
-            i++;
-        }
-        while (j < n2) {
-            ans += word2[j];
-            j++;
-        }
-
-        return ans;
+        return res;
     }
 };
