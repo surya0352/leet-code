a95390b9eef56ef8440fab6687437f0d453c2a02
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-07-26 23:46:32 +0530

    0258-add-digits Easy .cpp

--- C++/Easy/0258-add-digits/0258-add-digits.cpp
+++ C++/Easy/0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(num>=10)
-        {
-            int sum=0;
-            while(num>0)
-            {
-                int dig=num%10;
-                sum+=dig;
-                num/=10;
-            }
-            num=sum;
-        }
-        return num;
+        if (num == 0)
+            return num;
+        if (num % 9 == 0)
+            return 9;
+        
+            return num % 9;
     }
 };
