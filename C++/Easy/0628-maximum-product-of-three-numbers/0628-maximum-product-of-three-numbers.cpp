265dede9fe0ddaab489135fc77451a0c58565915
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-07-26 22:06:58 +0530

    0628-maximum-product-of-three-numbers Easy .cpp

--- C++/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ C++/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int first = INT_MIN, sec = INT_MIN, third = INT_MIN;
-         int smallest = INT_MAX, secondSmallest = INT_MAX;
-        for (int x : nums) {
-            if (x > first) {
-                third = sec;
-                sec = first;
-                first = x;
-            } else if (x > sec) {
-                third = sec;
-                sec = x;
-            } else if (x > third) {
-                third = x;
-            }
-            if (x <= smallest) {
-                secondSmallest = smallest;
-                smallest = x;
-            } 
-            else if (x < secondSmallest) {
-                secondSmallest = x;
-            }
-        }
-        return max(first*sec*third,smallest*secondSmallest*first);
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int pro1=nums[n-1]*nums[n-2]*nums[n-3];
+        int pro2=nums[0]*nums[1]*nums[n-1];
+        return max(pro1,pro2);
     }
 };
