450aea36f49793d4b6206a7db1ded9047b23fef4
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-07-31 01:10:26 +0530

    3014-minimum-number-of-pushes-to-type-word-i Easy .cpp

--- C++/Easy/3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ C++/Easy/3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.length();
-        int num = 0;
-        for (int i = 0; i < word.length(); i++) {
-            num += (i / 8) + 1;
+        int pushes = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            pushes += (i / 8) + 1;
         }
-
-        return num;
+        
+        return pushes;
     }
 };
