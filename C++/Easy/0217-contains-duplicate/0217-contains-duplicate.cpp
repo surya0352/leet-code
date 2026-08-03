861423f71b49b0a6bac879e9cf7c512ec676a64a
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-08-03 09:09:17 +0530

    0217-contains-duplicate Easy .cpp

---
+++ C++/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int x:nums)
+        {
+            freq[x]++;
+        }
+        for(auto x:freq)
+        {
+            if(x.second>1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
