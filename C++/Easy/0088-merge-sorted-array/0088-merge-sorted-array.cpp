32e7ae10c0df90f634f76e3eff534c807b135b96
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-08-19 15:25:30 +0530

    0088-merge-sorted-array Easy .cpp

--- C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ C++/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,25 @@
-    class Solution {
-    public:
-        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-            nums1.erase(nums1.begin() + m , nums1.end());
-            for (int i = 0; i < n; i++) {
-                nums1.push_back(nums2[i]);
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> c;
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                c.push_back(nums1[i]);
+                i++;
+            } else {
+                c.push_back(nums2[j]);
+                j++;
             }
-            sort(nums1.begin(),nums1.end());
         }
-    };
+        while (i < m) {
+            c.push_back(nums1[i]);
+            i++;
+        }
+        while (j < n) {
+            c.push_back(nums2[j]);
+            j++;
+        }
+        nums1=c;
+    }
+};
