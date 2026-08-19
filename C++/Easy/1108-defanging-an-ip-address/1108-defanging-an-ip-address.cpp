ecf8c457c9b61e4814dd6e171e484fe641f0bd19
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-08-20 00:26:09 +0530

    1108-defanging-an-ip-address Easy .cpp

--- C++/Easy/1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ C++/Easy/1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         string ans;
-        for(int i = 0; i < address.size(); i++){
-            if(address[i] == '.'){
-               ans.push_back('[');
-               ans.push_back(address[i]);
-               ans.push_back(']');
-            }
+        for (int i = 0; i < address.length(); i++) {
+            if (address[i] == '.')
+                ans = ans + "[.]";
             else
-            {
-                ans.push_back(address[i]);
-            }
+                ans = ans + address[i];
         }
         return ans;
     }
