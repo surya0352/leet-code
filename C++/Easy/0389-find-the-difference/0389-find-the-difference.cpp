0d7264da59168e4ee10c163f2aefa07b7e14dd8a
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-08-20 21:59:43 +0530

    0389-find-the-difference Easy .cpp

--- C++/Easy/0389-find-the-difference/0389-find-the-difference.cpp
+++ C++/Easy/0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> freq1;
-        for (char ch : s) {
-            freq1[ch]++;
-        }
-        unordered_map<char, int> freq2;
-        for (char ch : t) {
-            freq2[ch]++;
-        }
-        for (auto x : freq2) {
-            if (x.second != freq1[x.first]) {
-                return x.first;
-            }
-        }
-        return ' ';
+        int sum = 0;
+
+        for (char ch : t)
+            sum += ch;
+
+        for (char ch : s)
+            sum -= ch;
+
+        return (char)sum;
     }
 };
