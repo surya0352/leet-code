e04d745bbeb99e23905203296b92f60f971e9293
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-08-27 13:59:32 +0530

    3718-smallest-missing-multiple-of-k Easy .cpp

--- C++/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ C++/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int maxi = INT_MIN;
-        for (int x : nums) {
-            maxi = max(maxi, x);
+        int x = k;
+        while (find(nums.begin(), nums.end(), x) != nums.end()) {
+            x += k;
         }
-        int count=0;
-        for (int i = 1; i <= maxi; i++) {
-            if (i % k == 0) {
-                if (find(nums.begin(), nums.end(), i) == nums.end()) {
-                    return i;
-                }
-                else{
-                    count++;
-                }
-            }
-        }
-        return count*k+k;
+        return x;
     }
 };
