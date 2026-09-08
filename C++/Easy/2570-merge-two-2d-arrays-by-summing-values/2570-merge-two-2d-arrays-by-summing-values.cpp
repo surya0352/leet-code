9fcca4d8907bb400e311e311f225eedc3832fc03
Surya Kant Ray <surya.25b01010352@abes.ac.in>
2026-09-08 23:18:40 +0530

    2570-merge-two-2d-arrays-by-summing-values Easy .cpp

---
+++ C++/Easy/2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+                                    vector<vector<int>>& nums2) {
+        vector<vector<int>> output;
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+            if (nums1[i][0] == nums2[j][0]) {
+                vector<int> temp(2);
+                temp[0] = nums1[i][0];
+                temp[1] = nums1[i][1] + nums2[j][1];
+                output.push_back(temp);
+                i++;
+                j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                output.push_back(nums1[i]);
+                i++;
+            } else {
+                output.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while (i < n) {
+            output.push_back(nums1[i]);
+            i++;
+        }
+        while (j < m) {
+            output.push_back(nums2[j]);
+            j++;
+        }
+        return output;
+    }
+};
